35210c7b22305b48f2d1358bdac5c7589dfe1ac7
Soruov Roy <soruovfci@gmail.com>
2017-11-02 20:29:02 +0600

    Master layout assets file added

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>        
-    <title>@yield('title') - {{ config('app.name') }}</title>    
+    <title>@yield('title') - {{ config('app.name') }}</title>
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />        
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     
-
+	<!-- Admin Panel Main Style -->
+    <link href="{{ asset('css/admin/style.css') }}" rel="stylesheet">
 
 </head>
 <body>
     
+	<div class="admin-panel-wrapper">
+		
+	</div>
 
+	<!-- Admin Panel Default Scripts -->
+    <script type="text/javascript" src="{{ asset('js/admin/default-scripts.js') }}"></script>
+	
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+/**
+ * Admin CSS and JS
+ */
+mix.js('resources/assets/js/admin/default-scripts.js', 'public/js/admin');
+
+mix.sass('resources/assets/sass/admin/style.scss', 'public/css/admin');
